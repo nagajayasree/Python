2292d8e93369dffdc5097cd537023fdb152b8e31
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-09-21 22:10:59 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,30 @@
+print("Hello World")
+age = 28
+print(age)
+# name = input("What is your name? ")
+# print("Hello " + name)
+# birth_year = input("Birth year: ")
+# age = 2020 - int(birth_year)
+# print(age)
+
+# first = input("First: ")
+# second = input("Second: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.replace('for','4'))
+print(course)
+print('Python' in course)
+
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 % 3)
+print(10 / 3)
+print(10 // 3)
+print(10 ** 3)
+print(10 + 3 * 2)
+print((10 + 3) * 2)
